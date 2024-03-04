daf6e92a17404ac414d3d42099ec6491bf2fe404
lunafl1are <lunafl1are@protonmail.com>
2024-03-04 18:52:25 +0200

    added error messages

---
+++ src/main/java/com/netsmartz/mis/utils/Constant.java
@@ -0,0 +1,10 @@
+package com.netsmartz.mis.utils;
+
+public class Constant {
+
+    public static final String EMAIL_NOT_FOUND = "Invalid email id.";
+
+    public static final String Mail_SENDING_FAILED= "Failed to send mail. Please try again.";
+
+    public static final String FAILED_TO_SAVE_OR_UPDATE = "Failed to save or update";
+}
