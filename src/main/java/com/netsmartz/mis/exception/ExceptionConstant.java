30dea9df50ece4488ba92c8ddd1ae27928717d48
lunafl1are <lunafl1are@protonmail.com>
2024-03-14 18:52:25 +0200

    Fix issues with user sessions

---
+++ src/main/java/com/netsmartz/mis/exception/ExceptionConstant.java
@@ -0,0 +1,14 @@
+package com.netsmartz.mis.exception;
+
+public class ExceptionConstant {
+    public static final String BAD_CREDENTIALS = "Bad credentials.";
+    public static final String USER_NOT_FOUND = "Email doesn't exists in db.";
+    public static final String USER_DISABLED = "User is disabled. Please reach to admin.";
+
+    //Access forbidden message
+    public static final String ACCESS_FORBIDDEN = "You don't have enough permission to access this resource.";
+
+    //internal server error
+    public static  final String INTERNAL_SERVER_ERROR = "Something went wrong. Please contact admin.";
+
+}
